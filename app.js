b7ee851a560d1f93d5e5772399ce78e6e2ed99ba
Mesum Abbas <i.mesumabbas#@gmail.com>
2025-08-08 05:31:12 +0500

    Added cludinary c

--- app.js
+++ app.js
@@ -406,5 +406,5 @@ if (!fs.existsSync(uploadsDir)) {
 app.use('/uploads', express.static(uploadsDir));
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on PORT ${PORT}`);
 }); 
